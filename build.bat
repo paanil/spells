c3d2fb8eb655c52a074d09f40f36b1c5ebd61b49
Ilari <ilari@example.com>
2017-06-08 20:04:40 +0300

    Renamed JSON to C converter

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
-g++ -Wall -o json2c json2c.cpp json_spell_parser.cpp spell_iterator.cpp utils.cpp
+g++ -Wall jsonspells2c.cpp json_spell_parser.cpp spell_iterator.cpp utils.cpp -o jsonspells2c 
 
-@rem json2c spells.json
-json2c spells.json spells.autogen.inl
+@rem jsonspells2c spells.json
+jsonspells2c spells.json spells.autogen.inl
 
-g++ -Wall -o spells2csv spells2csv.cpp
+g++ -Wall spells2csv.cpp -o spells2csv 
 
 spells2csv all
