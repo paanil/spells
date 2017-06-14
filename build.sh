6a4bc5d0c3bee06071df6328fd851176d63eaa6a
Ilari Paananen <ilari.k.paananen@student.jyu.fi>
2017-06-14 15:28:46 +0300

    Build script for Linux

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 g++ -Wall -std=c++11 jsonspells2c.cpp json_spell_parser.cpp spell_iterator.cpp utils.cpp -o jsonspells2c 
 
-@rem jsonspells2c spells.json
-@rem jsonspells2c spells.json spells.autogen.inl
-jsonspells2c example.json spells.autogen.inl
+./jsonspells2c example.json spells.autogen.inl
 
 g++ -Wall -std=c++11 spells2csv.cpp -o spells2csv 
 
-spells2csv all
-
+./spells2csv all
